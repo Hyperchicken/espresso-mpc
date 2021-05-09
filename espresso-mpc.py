73b346d83e0bddbcc1cbd2091e23e73f62e8cada
Xascrimson <35974973+Xascrimson@users.noreply.github.com>
2021-05-09 22:19:31 +1000

    Update espresso-mpc.py

--- espresso-mpc.py
+++ espresso-mpc.py
@@ -142,7 +142,7 @@ def control_loop(dummy,state):
           print('h', mpc_mat['h_opt'])
           print('A', mpc_mat['A_opt'])
           print('B',b_opt)
-          sol = solvers.lp(mpc_mat['q_opt'], mpc_mat['G_opt'], mpc_mat['h_opt'], mpc_mat['A_opt'], b_opt), solver='cvxopt')
+          sol = solvers.lp(mpc_mat['q_opt'], mpc_mat['G_opt'], mpc_mat['h_opt'], mpc_mat['A_opt'], b_opt, solver='cvxopt')
           x_opt = numpy.array(sol['x'])
           u_mpc = x_opt[2*n,0] # Only use first control signal
 
